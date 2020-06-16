1829b0bb40413e7ef25333a238bc1f9e46589e3b
Ednaldo <martinsednaldo@outlook.com>
2020-06-16 11:01:31 -0300

    arquivo inicial para testar @GET e @POST de locais.

---
+++ src/routes/routesPlaces.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+
+router.route('/')
+    .get((req, res) => {
+        res.status(200).json(
+            {
+                message: 'Usando GET para rota de locais'
+            }
+        )
+    })
+    .post((req, res) => {
+        res.status(201).json(
+            {
+                message: 'Usando POST para rota de locais'
+            }
+        )
+    })
+
+router.route('/:place_id')
+    .get((req, res) => {
+        const id = req.params.place_id
+        res.status(200).json(
+            {
+                message: 'Usando GET para rota de locais com ID do local',
+                place_id: id
+            }
+        )
+    })
+
+module.exports = router
