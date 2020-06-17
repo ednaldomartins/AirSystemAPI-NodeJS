2c99e340c03ca5ecdadb3dea3be8dd1da5ccf104
Ednaldo <martinsednaldo@outlook.com>
2020-06-17 00:08:05 -0300

    implementação das rotas para POST e GET no database na tabela place.

--- src/routes/routesPlaces.js
+++ src/routes/routesPlaces.js
@@ -1,29 +1,41 @@
 const express = require('express')
 const router = express.Router()
+const insertDAO = require('../data/dao/insertDAO')
+const selectDAO = require('../data/dao/selectDAO')
 
 router.route('/places')
-    .get((req, res) => {
+    .get(async(req, res) => {
+        const result = await selectDAO.listTable('place')
+        console.log(result.rows)
         res.status(200).json(
             {
-                message: 'Usando GET para rota de locais'
+                message: 'Usando GET para rota de locais',
+                places: result.rows
             }
         )
     })
-    .post((req, res) => {
-        res.status(201).json(
+    .post(async(req, res) => {
+        const { cep, country, state, city } = req.body
+        const result = await insertDAO.insertPlace(cep, country, state, city)
+        console.log(result.rows)
+        return res.status(201).json(
             {
-                message: 'Usando POST para rota de locais'
+                message: 'POST: novo local criado',
+                place: result.rows
             }
         )
     })
 
-router.route('/places/:place_id')
-    .get((req, res) => {
-        const id = req.params.place_id
+router.route('/places/:city_name')
+    .get(async(req, res) => {
+        const cityName = req.params.city_name
+        const result = await selectDAO.getPlace(cityName)
+        console.log(result.rows)
         res.status(200).json(
             {
-                message: 'Usando GET para rota de locais com ID do local',
-                place_id: id
+                message: 'Usando GET para rota de locais com o nome do local',
+                place: result.rows,
+
             }
         )
     })
